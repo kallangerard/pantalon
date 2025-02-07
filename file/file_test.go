66053806c32b0331cceb5a39c7256190d167dfd3
Kallan Gerard <36556982+kallangerard@users.noreply.github.com>
2025-02-07 15:35:08 +0800

    add tests to ensure subdirectories are not searched after a pantalon file is found

--- file/file_test.go
+++ file/file_test.go
@@ -18,3 +18,43 @@ func TestWalkDir(t *testing.T) {
 
 	assert.Equal(t, expectedPaths, paths)
 }
+
+func TestWalkDir_NestedFileShouldBeFound(t *testing.T) {
+	root := path.Join("..", "testdata", "terraform", "nested-dir")
+	expectedPath := path.Join(root, "parent", "pantalon.yaml")
+
+	paths, err := FindPantalonFiles(root)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, expectedPath, paths[0])
+}
+
+func TestWalkDir_ChildDirectoriesShouldNotBeSearched(t *testing.T) {
+	root := path.Join("..", "testdata", "terraform", "nested-dir")
+	expectedPaths := []string{path.Join(root, "parent", "pantalon.yaml")}
+
+	paths, err := FindPantalonFiles(root)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, expectedPaths, paths)
+}
+
+func TestWalkDir_SiblingDirectories(t *testing.T) {
+	root := path.Join("..", "testdata", "terraform", "sibling-dir")
+	expectedPaths := []string{
+		path.Join(root, "a", "pantalon.yaml"),
+		path.Join(root, "b", "pantalon.yaml"),
+		path.Join(root, "c", "pantalon.yaml"),
+	}
+
+	paths, err := FindPantalonFiles(root)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, expectedPaths, paths)
+}
